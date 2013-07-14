60c0b145985b95b999193388d88c4b1a354e5b1c
Edward Look <edwlook@gmail.com>
2013-07-14 22:39:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var message = fs.readFileSync(index.html);
+  response.send(message.toString());
 });
 
 var port = process.env.PORT || 5000;
